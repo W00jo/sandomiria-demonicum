362d6b22c69a3345689115ecd6a30ee18f348da9
MinikLawa <dominik.pulawa@gmail.com>
2025-08-14 20:58:09 +0200

    Crossbow Shooting

--- src/GameManager.gd
+++ src/GameManager.gd
@@ -2,10 +2,13 @@ extends Node
 
 var score: int = 0
 var player_health: int = 100
+var ammo = 2
+var current_weapon = "crossbow"
+
 
 func _ready() -> void:
-    print("GameManager ready.")
+	print("GameManager ready.")
 
 func reset_game() -> void:
-    score = 0
-    player_health = 100
+	score = 0
+	player_health = 100
--- src/ui.gd
+++ src/ui.gd
@@ -1,26 +1,29 @@
 extends CanvasLayer
 
 
-var ammo = 0
-var current_weapon = "knife"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.animation_finished.connect(_on_AnimatedSprite2D_animation_finished)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Uderzanie nozem lub strzelanie z kuszy, po kazdym strzale zabiera amunicje
 func _process(delta):
-	if Input.is_action_just_pressed("ui_select"):
-		if current_weapon == "knife":
+	if Input.is_action_just_pressed("ui_attack"):
+		if GameManager.current_weapon == "knife":
 			$AnimatedSprite2D.play("knife_stab")
-		elif current_weapon == "crossbow":
-			if ammo > 0:
+		elif GameManager.current_weapon == "crossbow":
+			if GameManager.ammo > 0:
 				$AnimatedSprite2D.play("crossbow_shoot")
-				ammo -= 1
+				GameManager.ammo -= 1
+			else:
+				GameManager.current_weapon = "knife"
+				$AnimatedSprite2D.play("knife_idle")
+
 
 func _on_AnimatedSprite2D_animation_finished():
-	if current_weapon == "knife":
+	if GameManager.current_weapon == "knife":
 		$AnimatedSprite2D.play("knife_idle")
-	elif current_weapon == "crossbow":
+	elif GameManager.current_weapon == "crossbow":
 		$AnimatedSprite2D.play("crossbow_idle")
