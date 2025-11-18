e7dfc99ba76ac494af468829748b1e83c24c41e8
W00jo <w00jcikjakub@gmail.com>
2025-11-18 12:35:41 +0100

    importy

--- scenes/ui/hud/base_hud.gd
+++ scenes/ui/hud/base_hud.gd
@@ -1,13 +1,18 @@
 extends Node
 
 @onready var hud_container = $HUDLayer
+@onready var crosshair = $HUDlayer/Crosshair
 
-func spawn_player(character_type: String):
-	var player = load("res://characters/" + character_type + ".tscn").instantiate()
-	var hud = load("res://hud/" + character_type + "_hud.tscn").instantiate()
-	
-	add_child(player)
-	hud_container.add_child(hud)
-	
-	# Connect signals
-	hud.set_player(player)
+func _ready() -> void:
+	crosshair.position.x = hud_container.x / 2 - crosshair.size.x / 2
+	crosshair.position.y = hud_container.y / 2 - crosshair.size.y / 2
+
+#func spawn_player(character_type: String):
+	#var player = load("res://characters/" + character_type + ".tscn").instantiate()
+	#var hud = load("res://hud/" + character_type + "_hud.tscn").instantiate()
+	#
+	#add_child(player)
+	#hud_container.add_child(hud)
+	#
+	## Connect signals
+	#hud.set_player(player)
