5ba886ac209437185581e88ee9bd8e58ed20b36a
Jakub <94285273+W00jo@users.noreply.github.com>
2025-11-19 14:49:21 +0100

    Imports and base HUD layout

--- scenes/ui/hud/base_hud.gd
+++ scenes/ui/hud/base_hud.gd
@@ -1,13 +1,24 @@
 extends Node
 
-@onready var hud_container = $HUDLayer
+@onready var hud_container: CanvasLayer = $HUDLayer
+@onready var got_hit: TextureRect = $HUDLayer/GotHit
+@onready var crosshair: TextureRect = $HUDLayer/Crosshair
+@onready var direction_indicator: TextureRect = $HUDLayer/DirectionIndicator
+@onready var hitmarker: TextureRect = $HUDLayer/Hitmarkers
 
-func spawn_player(character_type: String):
-	var player = load("res://characters/" + character_type + ".tscn").instantiate()
-	var hud = load("res://hud/" + character_type + "_hud.tscn").instantiate()
-	
-	add_child(player)
-	hud_container.add_child(hud)
-	
-	# Connect signals
-	hud.set_player(player)
+func _ready():
+	crosshair.position.x = get_viewport().size.x / 2 - 32
+	crosshair.position.y = get_viewport().size.y / 2 - 32
+	hitmarker.position.x = get_viewport().size.x / 2 - 32
+	hitmarker.position.y = get_viewport().size.y / 2 - 32
+
+## Mysle nad czyms takim, aby podstawowy HUD byl dla kazdego
+#func spawn_player(character_type: String):
+	#var player = load("res://characters/" + character_type + ".tscn").instantiate()
+	#var hud = load("res://hud/" + character_type + "_hud.tscn").instantiate()
+	#
+	#add_child(player)
+	#hud_container.add_child(hud)
+	#
+	## Connect signals
+	#hud.set_player(player)
