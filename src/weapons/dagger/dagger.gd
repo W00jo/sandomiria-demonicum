726c9d18687c756f23754f678f21fa3263dd3600
Jakub <94285273+W00jo@users.noreply.github.com>
2025-12-13 17:55:42 +0100

    Add AnimationPlayer node to Dagger weapon

--- src/weapons/dagger/dagger.gd
+++ src/weapons/dagger/dagger.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
