130d53c98d6d57d41153c9a93111874c54abe790
Jakub <94285273+W00jo@users.noreply.github.com>
2026-01-17 01:01:20 +0100

    Nvm this time add main menu

---
+++ src/ui/menu/main_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var play_button = $BookContainer/LeftPage/MenuContainer/ContentVBox/MenuButtons/PlayButton
+@onready var settings_button = $BookContainer/LeftPage/MenuContainer/ContentVBox/MenuButtons/SettingsButton
+@onready var credits_button = $BookContainer/LeftPage/MenuContainer/ContentVBox/MenuButtons/CreditsButton
+@onready var quit_button = $BookContainer/LeftPage/MenuContainer/ContentVBox/MenuButtons/QuitButton
+
+func _ready():
+	pass
+
+func _on_play_button_pressed() -> void:
+	print("Play")
+	get_tree().change_scene_to_file("res://src/levels/tutorial.tscn")
+
+func _on_settings_button_pressed() -> void:
+	print("Settings")
+	# TODO: settings
+
+func _on_credits_button_pressed() -> void:
+	print("Credits")
+	# TODO: credits
+
+func _on_quit_button_pressed() -> void:
+	print("Quit")
+	get_tree().quit()
