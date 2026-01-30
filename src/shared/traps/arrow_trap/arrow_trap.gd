1cb4bab3d00f6372116cd08f47da904919818e20
MinikLawa <dominik.pulawa@gmail.com>
2026-01-30 13:03:57 +0100

    traps bugfix and traps sfx added

--- src/shared/traps/arrow_trap/arrow_trap.gd
+++ src/shared/traps/arrow_trap/arrow_trap.gd
@@ -6,8 +6,11 @@ func _physics_process(delta: float) -> void:
 	global_position -= (transform.basis.z * speed * delta)
 
 
+func ready():
+	print("urodziłam sie")
 
 func _on_body_entered(body: Node3D) -> void:
+	print("Strzała dotknęła: ", body.name)
 	if body.has_method("take_damage"):
 		body.take_damage(20)
 		print("trafiony zatopiony")
--- src/shared/traps/arrow_trap/trap.gd
+++ src/shared/traps/arrow_trap/trap.gd
@@ -3,7 +3,7 @@ extends StaticBody3D
 @export var arrow_scene: PackedScene
 
 @onready var spawn_point = $Barell
-
+@onready var sfx_shoot = $ShootSound
 
 
 
@@ -11,3 +11,6 @@ func _on_timer_timeout() -> void:
 	var new_arrow = arrow_scene.instantiate()
 	get_tree().current_scene.add_child(new_arrow)
 	new_arrow.global_transform = spawn_point.global_transform
+	
+	
+	sfx_shoot.play()
